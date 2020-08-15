365f232333edb6f3ff49f6a2abe7a31b088c86c9
Amr Ahmed <amr.abdalrahman94@gmail.com>
2020-08-16 01:44:33 +0200

    host to heroku

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const upload = require("./upload");
 
 module.exports = app => {
+
+    app.get("/", (req, res) => {
+        res.status(200).send({message: "please use /api/create post endpoint for testing"});
+    });
     app.use("/api", upload);
 };
